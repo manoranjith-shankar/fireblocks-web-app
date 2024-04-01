5accc49de2b268452c172e2a5765515e7314032e
manoranjith shankar <ma.ran4073@gmail.com>
2024-04-01 13:38:41 +0530

    [fix] backend jwt

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,29 @@
 
 import { useEffect, useState } from "react";
 import { useAppStore } from "./AppStore";
+import {Card, CardHeader, CardBody, CardFooter, Divider, Link, Image} from "@nextui-org/react";
+import { Button } from "@nextui-org/button";
+import { Snippet } from "@nextui-org/react";
+import { validateGuid } from "@/components/validateGuid";
 
 export default function Home() {
-  const { loggedUser, appStoreInitialized, initAppStore, loginToDemoAppServerStatus, loginToDemoAppServer } = useAppStore();
+
+  const [device, setDevice] = useState("");
+  const { loggedUser,
+    appStoreInitialized,
+     initAppStore,
+      loginToDemoAppServerStatus,
+       loginToDemoAppServer,
+        userId,
+        deviceId,
+        setAppMode,
+        generateNewDeviceId,
+        assignCurrentDevice,
+        assignDeviceStatus,
+        appMode,
+        walletId,
+        setDeviceId
+      } = useAppStore();
   const [initialized, setInitialized] = useState(false);
 
   useEffect(() => {
@@ -14,8 +34,11 @@ export default function Home() {
     }
   }, [loggedUser]);
 
-  console.log(appStoreInitialized, "1")
-  console.log(loginToDemoAppServerStatus, "2")
+  // console.log(appStoreInitialized, "1")
+  // console.log(loginToDemoAppServerStatus, "2")
+
+  const validDeviceId = validateGuid(deviceId);
+  // const isValidWalletId = validateGuid(walletId);
 
   useEffect(() => {
     if (initialized && loggedUser ) {
@@ -23,10 +46,58 @@ export default function Home() {
     }
   },[loggedUser, initialized])
 
+  const handleCreateWallet = () => {
+    setAppMode("SIGN_IN")
+  }
+
+  const handleAssignDevice = () => {
+      console.log("login status", loginToDemoAppServerStatus)
+      setDeviceId(deviceId)
+      assignCurrentDevice();
+      console.log("assigning device", assignDeviceStatus)
+  }
+
+  const handlegenerateNewDeviceId = () => {
+    generateNewDeviceId()
+  }
+
   return (
     <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
       loggedUser: {loggedUser ? loggedUser.email : "No user logged in"} <br />
-      {appStoreInitialized ? "App store initialized" : "App store not initialized"}
+      {appStoreInitialized ? "App store initialized" : "App store not initialized"} <br />
+      {loginToDemoAppServerStatus === "success" ? userId: "Not logging in to demo app server"}
+      <Card className="max-w-[500px]">
+      <CardHeader className="flex gap-3">
+        <div className="flex flex-col">
+          <p className="text-md">App Mode</p>
+          <p className="text-small text-default-500">select App Mode</p>
+        </div>
+      </CardHeader>
+      <Divider/>
+      <CardBody>
+      <div className="grid grid-cols-2">
+      <Button className="m-3 p-4" onClick={handleCreateWallet}>
+        Create new wallet
+      </Button>
+      <Button className="m-3 px-4">
+        Join existing Wallet
+      </Button>
+      </div> 
+      </CardBody>
+      <CardFooter>
+        <div className="flex flex-col">
+        <Snippet symbol= " " variant="bordered">
+          {deviceId}
+          </Snippet>
+          <Button className="m-4" onClick={handlegenerateNewDeviceId}>
+            Generate new Device Id
+          </Button>
+          <Button className="m-4" onClick={handleAssignDevice}>
+            Assign Device
+          </Button>
+          </div>
+        </CardFooter>
+    </Card>
     </section>
   );
 }
--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,37 +1,46 @@
 "use client"
 
-import React, { useEffect } from "react";
-import { useAppStore } from "@/app/AppStore";
-import LoginToDemoAppServer from "./DemoLogin";
-import { Button } from "@nextui-org/button";
+import React from "react";
+import {Modal, ModalContent, ModalHeader, ModalBody, ModalFooter, Button, useDisclosure, Checkbox, Input, Link} from "@nextui-org/react";
+import { Snippet } from "@nextui-org/react";
 
-const App = () => {
-  const { loggedUser, userId, initAppStore, loginToDemoAppServer, appStoreInitialized } = useAppStore();
-  console.log(loggedUser, '1')
-
-    if(loggedUser != null) {
-      console.log(appStoreInitialized, 'appstore')
-    }
-
-    const handleLogin = async () => {
-      if (!appStoreInitialized) {
-        await initAppStore();
-      } else {
-        await loginToDemoAppServer();
-      }
-      setTimeout(() => {
-        loginToDemoAppServer();
-      }, 1000)
-    }
+export default function Page({isJoinWallet, isCreateNewWallet}) {
+  const {isOpen, onOpen, onOpenChange} = useDisclosure();
 
   return (
-      <div className="justify-items-center">
-        <Button onClick={handleLogin}>
-          login
-        </Button>
-        {/* <div>{loggedUser ? <AppContent /> : <Login />}</div> */}
-      </div>
+    <>
+      <Button onPress={onOpen} color="primary">Open Modal</Button>
+      <Modal 
+        isOpen={isOpen}
+        onOpenChange={onOpenChange}
+        placement="top-center"
+      >
+        <ModalContent>
+          {(onClose) => (
+            <>
+              <ModalHeader className="flex flex-col gap-1">Create new Wallet</ModalHeader>
+              <ModalBody>
+                  Device Id
+                <Snippet symbol= " " variant="bordered">Device Id</Snippet>
+                <Input
+                  label="Password"
+                  placeholder="Enter your password"
+                  type="password"
+                  variant="bordered"
+                />
+              </ModalBody>
+              <ModalFooter>
+                <Button color="danger" variant="flat" onPress={onClose}>
+                  Generate New Device Id
+                </Button>
+                <Button color="primary" onPress={onClose}>
+                  Assign Device
+                </Button>
+              </ModalFooter>
+            </>
+          )}
+        </ModalContent>
+      </Modal>
+    </>
   );
-};
-
-export default App
+}
